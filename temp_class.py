b662fd20dd5b66d306e8633906fa7873d55c4848
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-01-11 18:10:31 +0300

    Модернизация кода
    Изменение классов в соответствии с задачей "Машинка", изменение размера

--- temp_class.py
+++ temp_class.py
@@ -2,7 +2,7 @@ import os, pygame, random
 
 clock = pygame.time.Clock()
 pygame.init()
-size = width, height = 300, 300
+size = width, height = 600, 95
 screen = pygame.display.set_mode(size)
 running = True
 fps = 60
@@ -20,35 +20,34 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Player(pygame.sprite.Sprite):
-    image_pl = load_image("creature.png")
+class Car(pygame.sprite.Sprite):
+    image = load_image("car2.png")
 
     def __init__(self, group):
         super().__init__(group)
-        self.image = Player.image_pl
+        self.image = Car.image
+        self.speed = 100
         self.rect = self.image.get_rect()
-        self.rect.y = height // 2 - self.image.get_height() // 2
         self.rect.x = width // 2 - self.image.get_width() // 2
+        self.rect.y = height // 2 - self.image.get_height() // 2
 
     def update(self, *args):
-        if args and args[0].type == pygame.KEYDOWN:
-            if args[0].key == 275:
-                self.rect.x += 10
-            if args[0].key == 276:
-                self.rect.x -= 10
-            if args[0].key == 273:
-                self.rect.y -= 10
-            if args[0].key == 274:
-                self.rect.y += 10
+        if self.rect.x + self.image.get_width() >= width:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.speed = -self.speed
+        elif self.rect.x <= 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.speed = -self.speed
+        self.rect.x += self.speed * clock.tick(fps) / 1000
 
-                
+
 all_sprites = pygame.sprite.Group()
-Player(all_sprites)
+Car(all_sprites)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        all_sprites.update(event)
-    screen.fill((0, 0, 0))
+    all_sprites.update()
+    screen.fill((255, 255, 255))
     all_sprites.draw(screen)
     pygame.display.flip()
